8acc74989d601ab1d6844e155f59b89322f04415
Fiddler46 <adhyaksh99@gmail.com>
2022-08-14 23:35:20 +0530

    Additional conditions for when adding members

--- commands/addMember.js
+++ commands/addMember.js
@@ -1,4 +1,4 @@
-const standupModel = require("../models/standup.model");
+const memberModel = require("../models/member.model");
 /**
  * !am - Adds a new member to the standup
  * NOTE: server admin can only preform this operation
@@ -14,37 +14,75 @@ module.exports = {
         "Ruh Roh! You need to mention **_at least_** one member as argument!"
       );
 
-    standupModel
-      .findById(message.guild.id)
-      .then((standup) => {
-        args.forEach((mention) => {
-          if (mention.startsWith("<@") && mention.endsWith(">")) {
-            mention = mention.slice(2, -1);
+      // console.log(message.author.id)
+      // message.channel.name
+    const newMember = new memberModel({m_id:message.author.id, projects: [message.channel.name]})
+    const projectMember = new memberModel({ projects: [message.channel.name] })
+    
+    memberModel.findById(message.author.id).then(member=>{
+      if(!member){
+        newMember.save((err)=>{
+          if(err) throw new Error("The member is NOT saved!")
+        })
+      }
+      else {
+        if(member.projects.includes(message.channel.name) == true) {
+          return message.channel.send(
+            "This user is already part of this project, please add a different user."
+          )
+        }
+        else {
+          projectMember.save((err) => {
+            if (err)
+              throw new Error("Project was NOT saved!")
+          })
+        } 
+      }
+    })
 
-            if (mention.startsWith("!")) mention = mention.slice(1);
 
-            const member = message.guild.members.cache.get(mention);
 
-            if (member && standup.members.indexOf(member.id) == -1)
-              standup.members.push(member.id);
-          }
-        });
+    // memberModel
+    //   .findById(message.guild.id)
+    //   .then((memberObject) => {
+    //     args.forEach((mention) => {
+    //       if (mention.startsWith("<@") && mention.endsWith(">")) {
+    //         mention = mention.slice(2, -1);
 
-        standup
-          .save()
-          .then(() => message.channel.send("Members updated :tada:"))
-          .catch((err) => {
-            console.err(err);
-            message.channel.send(
-              "Oh no :scream:! An error occured somewhere in the matrix!"
-            );
-          });
-      })
-      .catch((err) => {
-        console.error(err);
-        message.channel.send(
-          "Oh no :scream:! An error occured somewhere in the matrix!"
-        );
-      });
-  },
+    //         if (mention.startsWith("!")) mention = mention.slice(1);
+
+    //         const member = message.guild.members.cache.get(mention);
+
+    //         if (member && memberObject.m_id.indexOf(member.id) == -1) {
+    //           memberObject.m_id.push(member.id);
+    //           memberObject.projects.push(message.channel);
+    //         }
+    //         else {
+    //           memberObject.findOne({m_id: member}, (err, obj) => {
+    //             if (obj.projects.includes(message.channel)) {
+    //               obj.projects.push(message.channel);
+    //             } 
+    //           })
+    //         }
+              
+    //       }
+    //     });
+
+    //     memberObject
+    //       .save()
+    //       .then(() => message.channel.send("Members updated :tada:"))
+    //       .catch((err) => {
+    //         console.err(err);
+    //         message.channel.send(
+    //           "Oh no :scream:! An error occured somewhere in the matrix!"
+    //         );
+    //       });
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //     message.channel.send(
+    //       "Oh no :scream:! An error occured somewhere in the matrix!"
+    //     );
+    //   });
+  }
 };
