3f5e4c0776b1ad7acda27b8a1a6d4d8eae7315c0
Fiddler46 <adhyaksh99@gmail.com>
2022-08-26 16:20:42 +0530

    added in report model for weekly reports

---
+++ models/report.model.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const reportSchema = new Schema({
+    m_id: String,
+    m_name: String,
+    project: String,
+    content: {
+        timeOfCreation: Date,
+        features: [String],
+        enhancements: [String],
+        blockers: [String],
+        others: [String],
+    },
+});
+
+module.exports = model('Reports', reportSchema);
