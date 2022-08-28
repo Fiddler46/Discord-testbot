38e09bf90b7ea959bf08efebd2aaf72048698239
Fiddler46 <adhyaksh99@gmail.com>
2022-08-28 14:21:46 +0530

    changed content to be array of objects

--- models/report.model.js
+++ models/report.model.js
@@ -4,13 +4,13 @@ const reportSchema = new Schema({
     m_id: String,
     m_name: String,
     project: String,
-    content: {
+    content: [{
         timeOfCreation: Date,
         features: [String],
         enhancements: [String],
         blockers: [String],
         others: [String],
-    },
+    }],
 });
 
 module.exports = model('Reports', reportSchema);
