34a75a26c3158a4215c13ccc42be24223d1155de
Fiddler46 <adhyaksh99@gmail.com>
2022-08-29 12:05:02 +0530

    finds and updates doc if it's on same day

--- commands/replyPrompt.js
+++ commands/replyPrompt.js
@@ -1,89 +1,172 @@
 const standupModel = require("../models/standup.model");
+const endOfDay = require('date-fns/endOfDay');
+const startOfDay = require('date-fns/startOfDay');
 
 module.exports = {
   name: "reply",
   usage: "@<optional_serverId> [your-message-here]",
   description: "Reply to standup prompt",
-  execute(message, args) {
-    if (message.channel.type === "dm") {
-      if (!args.length || (args.length == 1 && args[0].startsWith("@")))
+  async execute(message, args) {
+    if (message.channel.type === "text") {
+      if (!args.length)
         return message.reply(
           "Ruh Roh! You must provide a response as a message. No one likes a :ghost: as a team member :exclamation: :anger:"
         );
+            if (message.author.id !== -1) {
+              let userscrum = args.splice(1).join(" ");
+              console.log(userscrum);
 
-      if (args[0].startsWith("@")) {
-        standupModel
-          .findById(args[0].slice(1))
-          .then((standup) => {
-            if (standup.members.indexOf(message.author.id) !== -1) {
-              standup.responses.set(
-                message.author.id,
-                args.splice(1).join(" ")
-              );
-              standup
-                .save()
-                .then(() => message.channel.send("Updated Response :tada:"))
-                .catch((err) => {
-                  console.error(err);
-                  message.channel.send(
-                    "Oh no :scream:! An error occured somewhere in the matrix!"
-                  );
-                });
+              let [p, f, e, b] = ["#p", "#f", "#e", "#b", "#o"];
+
+              let indices_features = [];
+              let indices_enhancements = [];
+              let indices_blockers = [];
+
+              let index_project = userscrum.indexOf(p);
+
+              let project = [];
+              index_project += 2;
+              while (
+                userscrum[index_project] != "#" &&
+                userscrum[index_project] != "\n"
+              ) {
+                project.push(userscrum[index_project]);
+                index_project++;
+              }
+              project.unshift("Project Name ==> ");
+              project = project.join("");
+
+              //Find all occurances for Features
+              let idx = userscrum.indexOf(f);
+              while (idx != -1) {
+                indices_features.push(idx);
+                idx = userscrum.indexOf(f, idx + 1);
+              }
+
+              //Find all occurances for Enhancements
+              idx = userscrum.indexOf(e);
+              while (idx != -1) {
+                indices_enhancements.push(idx);
+                idx = userscrum.indexOf(e, idx + 1);
+              }
+
+              //Find all occurances for Blockers
+              idx = userscrum.indexOf(b);
+              while (idx != -1) {
+                indices_blockers.push(idx);
+                idx = userscrum.indexOf(b, idx + 1);
+              }
+              
+              // Finding enhancements
+              let enhancements = [];
+              for (let i = 0; i < indices_enhancements.length; i++) {
+                enhancements.push(i + 1, ". ");
+                let ijk = indices_enhancements[i];
+                ijk += 2;
+                while (
+                  ijk < userscrum.length &&
+                  userscrum[ijk] != "#" &&
+                  userscrum[ijk] != "\n"
+                ) {
+                  enhancements.push(userscrum[ijk]);
+                  ijk++;
+                }
+                enhancements.push(" \n");
+              }
+              enhancements = enhancements.join("");
+
+              // Finding blockers
+              let blockers = [];
+              for (let i = 0; i < indices_blockers.length; i++) {
+                blockers.push(i + 1, ". ");
+                let ijk = indices_blockers[i];
+                ijk += 2;
+                while (
+                  ijk < userscrum.length &&
+                  userscrum[ijk] != "#" &&
+                  userscrum[ijk] != "\n"
+                ) {
+                  blockers.push(userscrum[ijk]);
+                  ijk++;
+                }
+                blockers.push(" \n");
+              }
+              blockers = blockers.join("");
+
+              // Finding features
+              let features = [];
+              for (let i = 0; i < indices_features.length; i++) {
+                features.push(i + 1, ". ");
+                let ijk = indices_features[i];
+                ijk += 2;
+                while (
+                  ijk < userscrum.length &&
+                  userscrum[ijk] != "#" &&
+                  userscrum[ijk] != "\n"
+                ) {
+                  features.push(userscrum[ijk]);
+                  //console.log(userscrum[ijk])
+                  ijk++;
+                }
+                features.push(" \n");
+              }
+              features = features.join("");
+              console.log(features, "features");
+              const d = new Date()
+
+              const newStandup = new standupModel({
+                m_id: message.author.id,
+                member: message.author.username,
+                reportTime: d,
+                project: message.channel.name,
+                scrum: userscrum,
+                features: features.split(" \n"),
+                enhancements: enhancements.split(" \n"),
+                blockers: blockers.split(" \n"),
+              });
+              
+              const standup = await standupModel.findOne({
+                 m_id: message.author.id, 
+                 project: message.channel.name, 
+                 reportTime: {
+                  $gte: startOfDay(new Date()),
+                  $lte: endOfDay(new Date())
+                } 
+              });
+              if (standup) {
+                await standup.updateOne({
+                  scrum: userscrum,
+                  features: features.split(" \n"),
+                  enhancements: enhancements.split(" \n"),
+                  blockers: blockers.split(" \n")
+              });
+               return message.channel.send(
+                 "Scrum Updated!"
+               );
+              }
+              else {
+                newStandup.save();
+                return message.channel.send(
+                  "New standup added!"
+                );
+                
+              }
+              
+              
+              // standup
+              //   .save()
+              //   .then(() => message.channel.send("Updated Response :tada:"))
+              //   .catch((err) => {
+              //     console.error(err);
+              //     message.channel.send(
+              //       "Oh no :scream:! An error occured somewhere in the matrix!"
+              //     );
+              //   });
             } else {
               message.channel.send(
                 "Ruh Roh! You must be a team member in this server standup to reply to the response!"
               );
             }
-          })
-          .catch((err) => {
-            console.error(err);
-            message.channel.send(
-              "Oh no :scream:! An error occured somewhere in the matrix!"
-            );
-          });
-      } else {
-        standupModel
-          .find()
-          .then((standups) => {
-            const userStandupList = standups.filter(
-              (standup) => standup.members.indexOf(message.author.id) !== -1
-            );
-
-            if (!userStandupList.length) {
-              message.channel.send(
-                "Ruh Roh! You must be a team member in ***__any__*** server standup to reply to the response!"
-              );
-            } else if (userStandupList.length > 1) {
-              message.channel.send(
-                "Ruh Roh! Looks like you're a member in multiple standup servers!\nTry `!reply @<serverId> [your-message-here]` if you would like to reply to a *specific* standup server.\n**_Crunchy Hint:_** To get the serverId for *any* server, right-click the server icon and press `Copy ID`.\nNote that you may need Developer options turned on. But like, what kinda developer uses a standup bot **_AND DOESN'T TURN ON DEVELOPPER SETTINGS_** :man_facepalming:"
-              );
-            } else {
-              let [standup] = userStandupList;
-              standup.responses.set(
-                message.author.id,
-                args.join(" ")
-              );
-              console.log(standup.responses.get(message.author.id));
-              standup
-                .save()
-                .then(() => message.channel.send("Updated Response :tada:"))
-                .catch((err) => {
-                  console.error(err);
-                  message.channel.send(
-                    "Oh no :scream:! An error occured somewhere in the matrix!"
-                  );
-                });
-            }
-          })
-          .catch((err) => {
-            console.error(err);
-            message.channel.send(
-              "Oh no :scream:! An error occured somewhere in the matrix!"
-            );
-          });
       }
-    } else {
-      return message.reply("private DM me with `!reply` :bomb:");
     }
-  },
-};
+}
